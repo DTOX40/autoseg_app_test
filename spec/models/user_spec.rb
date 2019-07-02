65dd69c2fa961eee973d5a66b871cfc79ddd6dbd
daniel <danielcandidosilwa@gmail.com>
2019-07-02 13:08:31 -0300

    ading create action to session controller with tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,21 @@ describe '#info' do
   it 'returns email and created_at' do
   	user.save!
   	allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
-	 expected(user.info).to eq("#{user.email} - #{user.created_at} - token: #{Devise.friendly_token}" ) 
+	 expected(user.info).to eq("#{user.email} - #{user.created_at} - token: #abc123xyzTOKEN" ) 
    end
-  end
-end 
+ end  
+
+
+   describe '#generate_authentication_token!' do
+ 		it 'generaters a unique auth token'
+
+ 		it 'generaters auth token when the curret auth token alredy has been taken' do	
+ 			allow(Devise).to receive(:friendly_token).and_return('#abc123tokenxyz', '#abc123tokenxyz', '#abc123456789')
+ 				existing_user = created(:user) 
+ 			user.generate_authentication_token!
+
+
+ 			expected(user.auth_token).not_to eq(existing_user.auth_token)
+    end
+  end 
+end
