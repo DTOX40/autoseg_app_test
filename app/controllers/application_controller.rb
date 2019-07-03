5e729692b514a408cb70d368dc2f175856344d80
daniel <danielcandidosilwa@gmail.com>
2019-07-03 18:49:53 -0300

    allowing to send name as attribute for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller
+  #protect_from_forgery with: :exception
+ 	
+ 	protected
+
+
+ 	def configure_permitted_parameters
+ 		devise_parameter_sanitizer.permit(:sing_up, keys:[:username])
+ 		devise_parameter_sanitizer.permit(:account_update, keys:[:username])
+ 	end
 end
